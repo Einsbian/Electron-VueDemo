a7e5c3847d3d08797e6773156af9a13d9ff75470
wangke <wangk@inedcn.com>
2019-03-14 10:26:28 +0800

    init

---
+++ electron-demo1/index.ts
@@ -0,0 +1,4 @@
+console.log('你好 TS！')
+var str: string = '你好 TS'
+
+export { str }
---
+++ electron-demo1/js/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+console.log('你好 TS！');
+var str = '你好 TS';
+exports.str = str;
